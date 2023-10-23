21494dcb8565ff078e0b209e3b847f9a32eeda5b
Vladiss <vladislav.kulakov.2002@mail.ru>
2023-10-23 16:17:07 +0300

    upd model.py, predict.py, train.py

--- preprocessor/predict.py
+++ preprocessor/predict.py
@@ -1,38 +1,29 @@
-
 import pickle
-from model import Model
 
 
 class Predict:
-    
-    
+
     def __init__(self):
         self.load_config()
-        
-    
+
     def load_config(self):
         self.config['model_name']
-    
-    
+
     def load_model(self):
         with open(self.config['model_name'], 'rb') as f:
             self.mdl = pickle.loads(f)
-    
-    
+
     def predict_model(self, image):
-        self.mdl.load_data(image, preprocessing_type = 'predict')
-        self.mdl.preprocessing_data(preprocessing_type = 'predict')
+        self.mdl.load_data(image, preprocessing_type='predict')
+        self.mdl.preprocessing_data(preprocessing_type='predict')
         self.mdl.train_model()
-        
-        
+
     def save_preprocessed_data(self):
         self.load_model()
         self.predict_model()
         self.save_preprocessed_data()
-        
-        
+
     def run(self):
         self.load_model()
         self.predict_model()
         self.save_preprocessed_data()
-        
--- preprocessor/train.py
+++ preprocessor/train.py
@@ -1,35 +1,29 @@
-import cv2
 import pickle
-from model import Model
+
+from keras_model.model import Model
 
 
 class Train:
-    
-    
+
     def __init__(self):
         self.load_config()
         self.mdl = Model()
-    
-    
+
     def load_config(self):
         pass
 
-    
     def create_model(self):
         self.mdl.init_model()
-    
-    
+
     def train_model(self, image):
-        self.mdl.load_data(image, preprocessing_type = 'train')
-        self.mdl.preprocessing_data(preprocessing_type = 'train')
+        self.mdl.load_data(image, preprocessing_type='train')
+        self.mdl.preprocessing_data(preprocessing_type='train')
         self.mdl.train_model()
-        
-        
+
     def save_model(self):
         with open('/artefacts/model.pkl', 'wb') as f:
             pickle.dumps(self.mdl, f)
-        
-        
+
     def run(self, image):
         self.create_model()
         self.train_model()
